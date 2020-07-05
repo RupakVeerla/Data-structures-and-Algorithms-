2acf70ae3bb54a351565f86377e25d5e5bd46476
RupakVeerla <54211182+RupakVeerla@users.noreply.github.com>
2020-07-05 19:49:12 +0530

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -9,4 +9,14 @@ def fibonacciIterative(n):
     s.append(s[i-2]+s[i-1])
   return s[n-2]+s[n-1]
 
-print(fibonacciIterative(40))
+def fibonacciRecursiveCache(n):
+  d = {}
+  def fibonacciRecursive(n):
+    if n in d: 
+      return d[n]
+    if n<2:
+      return n
+    d[n] = fibonacciRecursive(n-1) + fibonacciRecursive(n-2)
+  return fibonacciRecursive(n)
+
+print(fibonacciRecursiveCache(40))
